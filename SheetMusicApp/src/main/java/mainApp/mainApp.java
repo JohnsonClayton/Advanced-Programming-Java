1a9e13f33026f3655b108bec629af75c1e9a06f5
Clayton Johnson <cpjohnson@mavs.coloradomesa.edu>
2018-08-30 13:02:12 -0600

    Initialized SheetMusicApp

---
+++ SheetMusicApp/src/main/java/mainApp/mainApp.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author clayt
+ */
+import java.awt.*;
+import javax.swing.*;
+
+public class mainApp {
+    private static void createWindow() {
+        JFrame frame = new JFrame("Simple GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JLabel textLabel = new JLabel("I'm a label in the window", SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(300, 100));
+        
+        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+        
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        createWindow();
+    }
+    
+}
