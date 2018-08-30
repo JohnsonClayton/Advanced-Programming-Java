36c1f443b0b4722bb149b7ee915812bcd194415d
Clayton Johnson <cpjohnson@mavs.coloradomesa.edu>
2018-08-30 11:33:48 -0600

    updated to current

--- mavenproject1/src/main/java/com/github/JohnsonClayton/HelloWorld.java
+++ mavenproject1/src/main/java/com/github/JohnsonClayton/HelloWorld.java
@@ -10,5 +10,27 @@ package com.github.JohnsonClayton;
  * @author clayt
  */
 public class HelloWorld {
+    public static void main(String []args) throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello, world!");
+        helloWorld.sayMessage();
+    }
     
+    private String message = "";
+    
+    private void sayMessage() {
+        System.out.println(getMessage());
+    }
+    
+    public void setMessage(String _message) {
+        if (_message != null)
+            this.message = _message;
+        else {
+            throw new UnsupportedOperationException("Message cannot be null.");
+        }
+    }
+    
+    public String getMessage() {
+        return this.message;
+    }
 }
