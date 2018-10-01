66b5856ef04c82c28ba2ad55b3dcbc043ea3099b
Clayton Johnson <cpjohnson@mavs.coloradomesa.edu>
2018-09-30 19:14:38 -0600

    Adding Restaurant Class for List Assignment

---
+++ Restaurant/src/main/java/com/github/johnsonclayton/restaurant/Restaurant.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.johnsonclayton.restaurant;
+
+/**
+ *
+ * @author clayt
+ */
+public class Restaurant {
+    private Customers customers;
+    private MenuItems menu_items;
+    private int open_tables;
+    
+    public Restaurant() {
+        
+    }
+    
+    public static void main(String []args) {
+        System.out.println("Hello, world!");
+    }
+    
+}
